87cbe9b135ca163e74676a9980c9778e1ef52925
PengChao <deeplearning@vip.qq.com>
2018-10-14 02:30:27 +0800

    NEW: add UCHIYAMARKERS to project

---
+++ source/mylib/mycam.h
@@ -0,0 +1,35 @@
+/*
+	This file is part of UCHIYAMARKERS, a software for random dot markers.
+	Copyright (c) 2011 Hideaki Uchiyama
+
+	You can use, copy, modify and re-distribute this software
+	for non-profit purposes.
+*/
+
+#ifndef MYCAM_H
+#define MYCAM_H
+
+#include <opencv/cv.h>
+#include <opencv/cxcore.h>
+#include <opencv/highgui.h>
+#include <opencv/cvwimage.h>
+
+class MyCam
+{
+public:
+	MyCam();
+	~MyCam();
+
+	bool Get(IplImage* dst);
+	bool Init(const int iw=640, const int ih=480);
+
+private:
+	CvCapture *m_cap;
+
+public:
+	int w,h;
+};
+
+
+
+#endif
